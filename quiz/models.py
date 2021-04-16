30972e7549da3f0bf4a8f52ac8f268059caeca02
yusseef <yussef.raouf11@gmail.com>
2021-04-16 06:51:04 +0200

    configure admin panel

--- quiz/models.py
+++ quiz/models.py
@@ -1,34 +1,43 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
 class Category(models.Model):
     name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
 
-class Quizzes(model.Model):
+
+class Quizzes(models.Model):
+
     class Meta:
         verbose_name = _("Quiz")
         verbose_name_plural = _("Quizzes")
         ordering = ['id']
-    
-    title = models.CharField(max_length=255, default=_("New Quiz"), verbose_name = _("Quiz title"))
-    category = models.ForeignKey(Category, default=1, on_delete=models.DO_NOTHING())
+
+    title = models.CharField(max_length=255, default=_(
+        "New Quiz"), verbose_name=_("Quiz Title"))
+    category = models.ForeignKey(
+        Category, default=1, on_delete=models.DO_NOTHING)
     date_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
 
+
 class Updated(models.Model):
-    date_update = models.DateTimeField(verbose_name=_("Last Updated"), auto_now=True)
+
+    date_updated = models.DateTimeField(
+        verbose_name=_("Last Updated"), auto_now=True)
 
     class Meta:
         abstract = True
 
+
 class Question(Updated):
-      class Meta:
+
+    class Meta:
         verbose_name = _("Question")
         verbose_name_plural = _("Questions")
         ordering = ['id']
@@ -42,25 +51,37 @@ class Question(Updated):
     )
 
     TYPE = (
-        (0, _('Multiple Choice'))
+        (0, _('Multiple Choice')),
     )
 
-    quiz = models.ForeignKey(Quizess, related_name='question', on_delete=models.DO_NOTHING())
-    technique = models.IntegerField(choices=TYPE, default = 0, verbose_name=_("Type of Question"))
+    quiz = models.ForeignKey(
+        Quizzes, related_name='question', on_delete=models.DO_NOTHING)
+    technique = models.IntegerField(
+        choices=TYPE, default=0, verbose_name=_("Type of Question"))
     title = models.CharField(max_length=255, verbose_name=_("Title"))
-    difficulty = models.IntegerField(choices=SCALE, default=0, verbose_name=_("Difficulty"))
-    date_created = models.DateTimeField(auto_now_add=True, verbose_name=_('Date Created'))
-    is_active = models.BooleanField(default=False, verbose_name=_('Active status')) 
+    difficulty = models.IntegerField(
+        choices=SCALE, default=0, verbose_name=_("Difficulty"))
+    date_created = models.DateTimeField(
+        auto_now_add=True, verbose_name=_("Date Created"))
+    is_active = models.BooleanField(
+        default=False, verbose_name=_("Active Status"))
 
     def __str__(self):
         return self.title
 
+
 class Answer(Updated):
+
     class Meta:
         verbose_name = _("Answer")
         verbose_name_plural = _("Answers")
         ordering = ['id']
-    
-    question = models.ForeignKey(Question, related_name='answer', on_delete=DO_NOTHING())
-    answer_text = models.CharField(max_length=255, verbose_name=_('Answer Text'))
-    is_right = models.BooleanField(default = False)
+
+    question = models.ForeignKey(
+        Question, related_name='answer', on_delete=models.DO_NOTHING)
+    answer_text = models.CharField(
+        max_length=255, verbose_name=_("Answer Text"))
+    is_right = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.answer_text
