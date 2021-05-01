5a8b7848878098ae8c00df980568652a072621d3
yusseef <yussef.raouf11@gmail.com>
2021-05-01 13:37:26 +0200

    add header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import './App.css';
+import Header from './components/framework/Header'
 
 function App() {
   return (
-    <React.Fragment>
-        <div>Hello world</div>
+    <>
+        <Header />
 
-    </React.Fragment>
+    </>
     
   );
 }
---
+++ frontend/src/components/framework/Header.js
@@ -0,0 +1,35 @@
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles"
+const useStyles = makeStyles((theme) => ({
+    root:{
+        flexGrow: 1,
+
+    },
+    toolbarTitle:{
+        flexGrow: 1,
+    }
+}))
+
+export default function Header(){
+    const classes = useStyles();
+    return(
+        <div className={classes.root}>
+            <AppBar
+            position= "static"
+            color= "default"
+            elevation={0}
+            className={classes.appBar}>
+                <Toolbar>
+                    <Typography
+                        variant="h6"
+                        color="inherit"
+                        noWrap
+                        className={classes.toolbarTitle}
+                    >
+                        Quizzes
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </div>
+    )
+}
